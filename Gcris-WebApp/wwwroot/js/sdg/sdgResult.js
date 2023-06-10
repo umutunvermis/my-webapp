b84245191a36d2c7777525079ad219b3dc661af7
umutunvermis <umutunvermis@gmail.com>
2023-06-10 18:38:08 +0300

    finalized version of sdg page

--- Gcris-WebApp/wwwroot/js/sdg/sdgResult.js
+++ Gcris-WebApp/wwwroot/js/sdg/sdgResult.js
@@ -3,6 +3,26 @@
 
 // Write your JavaScript code.
 
+var sdgMap = {
+    goal1: `GOAL 1: No Poverty: `,
+    goal2: `GOAL 2: Zero Hunger: `,
+    goal3: `GOAL 3: Good Health and Well-being: `,
+    goal4: `GOAL 4: Quality Education: `,
+    goal5: `GOAL 5: Gender Equality: `,
+    goal6: `GOAL 6: Clean Water and Sanitation: `,
+    goal7: `GOAL 7: Affordable and Clean Energy: `,
+    goal8: `GOAL 8: Decent Work and Economic Growth: `,
+    goal9: `GOAL 9: Industry, Innovation and Infrastructure: `,
+    goal10: `GOAL 10: Reduced Inequality: `,
+    goal11: `GOAL 11: Sustainable Cities and Communities: `,
+    goal12: `GOAL 12: Responsible Consumption and Production: `,
+    goal13: `GOAL 13: Climate Action: `,
+    goal14: `GOAL 14: Life Below Water: `,
+    goal15: `GOAL 15: Life on Land: `,
+    goal16: `GOAL 16: Peace and Justice Strong Institutions: `,
+    goal17: `GOAL 17: Partnerships to achieve the Goal: `
+}
+
 function loadPublicationResult(title, publications){
 
     console.log(publications);
@@ -27,9 +47,9 @@ function buildPublicationResultDiv(publication) {
         if (key.substr(0, 4) == (`goal`)) {
             console.log(key, val);
             if (val / publication.len > 0.1)
-                var temp = '<h5 id="goal" class="display-8" style="color:green">&nbsp&nbsp&nbsp&nbsp' + key + ': &nbsp&nbsp&nbsp' + val / publication.len + '</h5>';
+                var temp = '<h5 id="goal" class="display-8" style="color:green">&nbsp&nbsp&nbsp&nbsp' + sdgMap[key] + '&nbsp&nbsp&nbsp' + val / publication.len + '</h5>';
             else
-                var temp = '<h5 id="goal" class="display-8">&nbsp&nbsp&nbsp&nbsp' + key + ': &nbsp&nbsp&nbsp' + val / publication.len + '</h5>';
+                var temp = '<h5 id="goal" class="display-8">&nbsp&nbsp&nbsp&nbsp' + sdgMap[key] + '&nbsp&nbsp&nbsp' + val / publication.len + '</h5>';
             $("#publication-result").append(temp);
         }
     });
